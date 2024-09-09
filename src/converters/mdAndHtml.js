c54924a10b02ea3e7475ddb0d0350662350fbc4d
wheelercj <chrisw289@gmail.com>
2024-09-09 15:55:11 -0700

    Create HTML to markdown & HTML converter

---
+++ src/converters/mdAndHtml.js
@@ -0,0 +1,180 @@
+/*
+   Copyright 2024 Chris Wheeler
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import { getSetting } from '../common.js';
+import { absolutizeElementUrls } from './utils/urls.js';
+import { removeHiddenElements, isInlineText } from './utils/html.js';
+import { newEscape, MdConverter } from './md.js';
+
+/**
+ * htmlToMdAndHtml converts an HTML fragment to markdown and HTML. Anything that renders
+ * well as markdown is converted, and the rest is left as HTML.
+ * @param {DocumentFragment} frag
+ * @returns {Promise<string>}
+ */
+export async function htmlToMdAndHtml(frag) {
+    removeHiddenElements(frag, document);
+    absolutizeElementUrls(frag, location.Href);
+
+    const ctx = {
+        locationHref: location.href,
+        document: document,
+        omitNav: await getSetting('omitNav'),
+        omitFooter: await getSetting('omitFooter'),
+        indent: '',
+
+        mdSubBrackets: await getSetting('mdSubBrackets'),
+        mdBulletPoint: await getSetting('mdBulletPoint'),
+        mdYoutube: await getSetting('mdYoutube'),
+    };
+
+    /** @type {function(string): string} */
+    ctx.escape = newEscape(ctx.mdSubBrackets);
+
+    if (isInlineText(frag.childNodes)) {
+        ctx.dontTrimText = true;
+    }
+
+    return mdAndHtmlConverter
+        .convertNodes(ctx, frag.childNodes)
+        .trim()
+        .replaceAll(/\n{3,}/g, '\n\n')
+        + '\n';
+}
+
+/**
+ * @typedef {function(object, Element): string} ElementConverter
+ */
+
+export class MdAndHtmlConverter extends MdConverter {
+    // [HTML elements reference | MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element)
+
+    constructor() {
+        super();
+    }
+
+    /** @type {ElementConverter} */
+    convertFIGURE(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertCITE(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertSMALL(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertSUB(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertSUP(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertU(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertAREA(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertAUDIO(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertIMG(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertMAP(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertVIDEO(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertEMBED(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertIFRAME(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertOBJECT(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertPICTURE(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertPORTAL(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertSVG(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertMATH(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertCANVAS(ctx, el) {
+        return el.outerHTML;
+    }
+
+    /** @type {ElementConverter} */
+    convertTABLE(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertMETER(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+
+    /** @type {ElementConverter} */
+    convertDETAILS(ctx, el) {
+        return '\n\n' + el.outerHTML + '\n\n';
+    }
+}
+
+const mdAndHtmlConverter = new MdAndHtmlConverter();

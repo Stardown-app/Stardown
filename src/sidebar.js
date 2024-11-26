9b765b502ca61748c78e8138730d7e985ea9de2c
wheelercj <chrisw289@gmail.com>
2024-11-26 01:38:47 -0800

    Enforce notepad character and save time limits
    Closes #164.

--- src/sidebar.js
+++ src/sidebar.js
@@ -14,10 +14,18 @@
    limitations under the License.
 */
 
-import { browser } from './browserSpecific.js';
+import { browser, sleep } from './browserSpecific.js';
 import { getSetting } from './getSetting.js';
 
 const notepad = document.getElementById('notepad');
+const MAX_CHARS = 8100; // sync storage character limit: https://developer.chrome.com/docs/extensions/reference/api/storage#property-sync-sync-QUOTA_BYTES_PER_ITEM:~:text=quota_bytes_per_item
+notepad.setAttribute('maxlength', MAX_CHARS);
+// if we switch to local storage to save the notepad, set MAX_CHARS to 10485000
+
+const charCount = document.getElementById('char-count');
+
+const SAVE_DELAY = 500; // milliseconds // sync storage time limit: https://developer.chrome.com/docs/extensions/reference/api/storage#property-sync-sync-MAX_WRITE_OPERATIONS_PER_MINUTE
+let lastEditTime = 0; // milliseconds
 
 browser.commands.getAll().then(cmds => {
     const copySelectionShortcut = cmds.find(
@@ -29,14 +37,18 @@ browser.commands.getAll().then(cmds => {
 // load the notepad content when the page loads
 getSetting('notepadContent').then(content => {
     notepad.value = content || '';
+    updateCharacterCount();
 });
 
 // save the notepad content when it changes
-notepad.addEventListener('input', () => {
-    browser.storage.sync.set({ notepadContent: notepad.value });
+notepad.addEventListener('input', async () => {
+    lastEditTime = Date.now();
+    updateCharacterCount();
+    charCount.setAttribute('style', 'color: black;');
+    await saveNotepad();
 });
 
-browser.runtime.onMessage.addListener(message => {
+browser.runtime.onMessage.addListener(async message => {
     if (message.destination !== 'sidebar') {
         return;
     }
@@ -44,15 +56,37 @@ browser.runtime.onMessage.addListener(message => {
     switch (message.category) {
         case 'sendToNotepad':
             const newText = '\n\n' + message.text.trim() + '\n\n';
+            // if the new text to add would go over the character limit
+            if (newText.length + notepad.value.length > MAX_CHARS) {
+                // ignore the new text and turn the character counter red
+                charCount.setAttribute('style', 'color: red;');
+                return;
+            }
+            charCount.setAttribute('style', 'color: black;');
 
             const before = notepad.value.slice(0, notepad.selectionStart).trimEnd();
             const after = notepad.value.slice(notepad.selectionEnd).trimStart();
 
             notepad.value = (before + newText + after).trim();
-            browser.storage.sync.set({ notepadContent: notepad.value });
+            lastEditTime = Date.now();
+            updateCharacterCount();
+            await saveNotepad();
             break;
         default:
             console.error(`Unknown message category: ${message.category}`);
             throw new Error(`Unknown message category: ${message.category}`);
     }
 });
+
+async function saveNotepad() {
+    await sleep(SAVE_DELAY);
+    if (lastEditTime + SAVE_DELAY > Date.now()) {
+        return;
+    }
+
+    browser.storage.sync.set({ notepadContent: notepad.value });
+}
+
+function updateCharacterCount() {
+    charCount.textContent = `${notepad.value.length}/${MAX_CHARS} characters`;
+}

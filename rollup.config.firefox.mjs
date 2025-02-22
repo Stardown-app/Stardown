469a19d97a7e969137b1e7d392031a22f9840440
wheelercj <chrisw289@gmail.com>
2025-02-22 14:53:47 -0800

    Add Jonathan Chua to the license

--- rollup.config.firefox.mjs
+++ rollup.config.firefox.mjs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2024 Chris Wheeler
+   Copyright 2024 Chris Wheeler and Jonathan Chua
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

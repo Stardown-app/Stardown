c04a1ebbc8d2b2a705feb083e03a10a06405f4a0
wheelercj <chrisw289@gmail.com>
2025-02-28 12:45:41 -0800

    Validate Stardown's version format

--- src/settings.js
+++ src/settings.js
@@ -18,13 +18,19 @@ import { browser, sleep, getShortcutInstructions } from './browserSpecific.js';
 import { getSetting } from './getSetting.js';
 
 /**
- * VERSION is Stardown's version. The value must match one of the regex patterns below.
- * This variable exists because the "version" properties in the manifests only support
- * stable release versions.
+ * VERSION is Stardown's version. The value must match one of the regex patterns below;
+ * you might want to copy & paste this block of code into a REPL to check. This variable
+ * exists because the "version" properties in the manifests only support stable release
+ * versions.
  */
 const VERSION = 'v2.0.0-alpha.2411120735';
 const stableReleaseTagPattern = /^v\d+\.\d+\.\d+$/;
 const prereleaseTagPattern = /^v\d+\.\d+\.\d+-(?:alpha|beta)\.\d{10}$/; // the last 10 digits are YYMMDDhhmm
+if (!stableReleaseTagPattern.test(VERSION) && !prereleaseTagPattern.test(VERSION)) {
+    throw "Stardown's version must match either of two regular expressions";
+} else {
+    console.log("The version's format is correct");
+}
 
 const manifest = browser.runtime.getManifest();
 document.querySelector('#versionNumber').innerHTML = VERSION;

383891e6d52a94992c58100406651a16297a8492
Jonathan <jon.chua51@gmail.com>
2025-05-28 13:05:22 -0700

    login method will facilitate the window transition to the github oauth page

--- src/github.js
+++ src/github.js
@@ -16,4 +16,17 @@ export class GitHubClient {
         /** @private @type {string|null} */
         this.token = null;
     }
+
+    /**
+     * Initiates the OAuth login flow by redirecting the user to GitHub.
+     * @returns {void}
+     */
+    login() {
+        const scope = "repo read:user";
+        const state = crypto.randomUUID(); // Prevent CSRF
+        localStorage.setItem("oauth_state", state);
+
+        const url = `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${encodeURIComponent(this.redirectUri)}&scope=${scope}&state=${state}`;
+        window.location.href = url;
+    }
 }
